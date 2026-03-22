a78677d98c350d1948fdb8302dee8e1f71a617ba
hell-walk <aadity0213@gmail.com>
2026-03-22 23:30:39 +0530

    second commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,50 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import { TypeAnimation } from 'react-type-animation';
+import Navbar from "~/components/navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "constants/ResumeCard";
+// import type {Resume} from 'types/index d';
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "neural-ai" },
+    { name: "description", content: "Smart ai to make your resume job worthy" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/public/images/bg-main.svg')] bg-cover">
+      <Navbar />
+    <section className="main-section">
+          <div className="page-heading">
+            <h1>Track Your Application And Get Resume Rating</h1>
+            <h2> <TypeAnimation
+                sequence={[
+                  'Review your submissions and check AI-powered Feedback',
+                  500,          // pause before dots start
+                  'Review your submissions and check AI-powered Feedback.',
+                  500,
+                  'Review your submissions and check AI-powered Feedback..',
+                  500,
+                  'Review your submissions and check AI-powered Feedback...',
+                  2000
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+            /></h2>
+          </div>
+        {resumes.length > 0 && (
+            <div className="resumes-section">
+                {resumes.map((resume) => {
+                    return (
+                        <ResumeCard key={resume.id} resume={resume}/>
+                    );
+                })}
+            </div>
+        )}
+    </section>
+
+
+
+  </main>
 }
